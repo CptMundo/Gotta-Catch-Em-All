4456da96096258a5c60855cb7e92133343c7aaef
CptMundo <>
2023-09-09 11:49:45 +0000

    Styled index.html and created script for Rules pop-up

---
+++ assets/js/script-index.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("popup-button");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementById("close-button");
+const overlay = document.getElementById("overlay");
+
+button.addEventListener("click", function () {
+    popup.style.display = "block";
+    overlay.style.display = "block"; // Show the overlay
+});
+
+closeButton.addEventListener("click", function () {
+    popup.style.display = "none";
+    overlay.style.display = "none"; // Hide the overlay
+});
